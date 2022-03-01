f87bba5272da41a032ad2f4da7b754cdd2bd38bc
Sébastien <draik92@hotmail.com>
2022-02-28 21:44:32 -0500

    COMMIT _ Refactoring Tilepalette
    Ruletile bugs in build.
    Started creating gameobject tiles to replace old ones.

---
+++ Assets/Scripts/Enemy AI/Basic Enemy AI/Behaviours/Movement/OnHitMoveBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHitMoveBehaviour : AbstractBehaviour
+{
+    public override void Behaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool ChildSpecificValidations()
+    {
+        throw new System.NotImplementedException();
+    }
+}
