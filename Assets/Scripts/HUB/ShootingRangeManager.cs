80a700b95a9c04b6ee619a05663c598bb43f3a95
Sébastien <draik92@hotmail.com>
2022-03-17 02:28:48 -0400

    COMMIT _ Task 09-04: Shooting Range
    ... Ajout d'un Scriptable servant de data structure : array linéaire
    ... ... Utilisé pour les practice targets
    ... ... Sera aussi utilisé pour les armes disponibles au champ de tir
    ... ... Permet l'utilisation du scriptable lors de l'implementation du combat avec monstres dans le hub via le musé

--- Assets/Scripts/HUB/ShootingRangeManager.cs
+++ Assets/Scripts/HUB/ShootingRangeManager.cs
@@ -1,41 +1,108 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using Pathfinding;
 
+[System.Serializable]
 public class ShootingRangeManager : MonoBehaviour
 {
     // SECTION - Field ===================================================================
     [Header("Shooting Range Variables")]
-    [SerializeField] private Transform myCenter;
-    [SerializeField] private float rangeX;
-    [SerializeField] private float rangeZ;
+    [SerializeField] private Transform shootingRangeCenter;
+    [SerializeField] private int rangeX;
+    [SerializeField] private int rangeZ;
+                     private GameObject[,] myGrid;
+                     [SerializeField] private Vector3Int pZero = new Vector3Int();
 
     [Header("Practice Targets")]
-    [SerializeField] private ShootingRangeEnemyListSO myPracticePrefabsSO;
-    private List<GameObject> myPracticeTargets;
-    private Transform myShootingRangeCenter;
+    [SerializeField] private GameObject[] defaultPracticeTargetPrefabs;
+    [SerializeField] private ArrayLinearGameObjectSO practiceTargetsSO;
+                     //private ArrayLinearGeneric<GameObject> myArrayGeneric_PracticeTarget;
+    [SerializeField] private List<GameObject> myPracticeTargetInstances;
+
+
+    [Header("Weapon Rack")]
+    [SerializeField] private WeaponSO[] defaultWeapons;
+
+    private const string myAStarString = "AStar";
+    private AstarPath myAstarPath;
 
 
     // SECTION - Property ===================================================================
 
 
-    // SECTION - Method ===================================================================
-    public void StartShootingRange()
+    // SECTION - Method - Unity Specific ===================================================================
+
+    private void Start()
+    {
+        myGrid = new GameObject[rangeX, rangeZ];
+
+        // Note
+        //      - If even number, left side will 1 shorter than right side
+        pZero.x = -(rangeX >> 1);
+        pZero.z = -(rangeZ >> 1);
+
+        SetAstarPathAndScan();
+
+        SetArrayLinearGenerics();
+    }
+
+
+    // SECTION - Method - Utility Specific ===================================================================
+    private void SetArrayLinearGenerics()
+    {
+        if (practiceTargetsSO.IsEmpty)
+            practiceTargetsSO.Copy(defaultPracticeTargetPrefabs);
+
+        //if (defaultPracticeTargetPrefabs != null && myWeaponsSO != null)
+            //myWeaponsSO.Copy(defaultWeapons);
+    }
+
+    private void SetAstarPathAndScan()
+    {
+        myAstarPath = GameObject.Find(myAStarString).GetComponent<AstarPath>();
+
+        myAstarPath.data.gridGraph.center = shootingRangeCenter.position;
+        myAstarPath.data.gridGraph.center.y = -1.0f; // Must be at ground level
+
+        myAstarPath.data.gridGraph.SetDimensions(rangeX * 2, rangeZ * 2, myAstarPath.data.gridGraph.nodeSize);
+        myAstarPath.Scan();
+    }
+
+    public void StartRandomPlacementShootingRange()
     {
         // Clear any leftovers
-        myPracticeTargets.Clear();
+        foreach (GameObject practiceTarget in myPracticeTargetInstances)
+            Destroy(practiceTarget);
 
-        float x = myCenter.position.x;
-        float z = myCenter.position.z;
+        myPracticeTargetInstances.Clear();
 
+        if (!practiceTargetsSO.IsEmpty)
+            for (int index = 0; index < practiceTargetsSO.Length; index++)
+                OnGridInstantiate(practiceTargetsSO.GetElement(index));
+    }
 
+    private void OnGridInstantiate(GameObject practiceTarget)
+    {
+        if (practiceTarget == null)
+            return;
 
-        foreach (GameObject practiceTarget in myPracticePrefabsSO.myEnemyList)
+        int x = Random.Range(0, rangeX);
+        int z = Random.Range(0, rangeZ);
+
+        if (myGrid[x, z] != null)
         {
-            //x = Random.Range();
-            //z = Random.Range();
-
-            myPracticeTargets.Add(Instantiate(practiceTarget, GameObject.Find("--------------------- WORLD").transform));
-            myPracticeTargets[myPracticeTargets.Count].transform.position = new Vector3(x, 0.0f, z);
+            OnGridInstantiate(practiceTarget);
+            return;
         }
+
+        int posX = pZero.x + x;
+        int posZ = pZero.z + z;
+
+        myGrid[x, z] = Instantiate(practiceTarget, shootingRangeCenter.transform);
+        myGrid[x, z].transform.localPosition = new Vector3(posX, 0.0f, posZ);
+        myGrid[x, z].transform.parent = null;
+        
+        myPracticeTargetInstances.Add(myGrid[x, z]);
     }
 }
