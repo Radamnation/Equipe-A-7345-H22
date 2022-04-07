9560c3f00ff9aaab675c66434449b6334e415701
Sébastien <draik92@hotmail.com>
2022-04-06 23:45:02 -0400

    COMMIT _ Task 34-01 & Task 33-04
    Task 34-01: Complete sprites for enemies
    ... Fire Spitter: Nord, East, Sud, Mort
    ... Mimic Exploding barrel: Nord, Est
    ... Imp: TOUT
    
    Task 33-04: Destroy Quit Gargoyle #if UNITY_WEBGL
    ... Détruit la QUIT gargoyle si application lancé en webgl

--- Assets/Scripts/Utility/DelegateToGameManager.cs
+++ Assets/Scripts/Utility/DelegateToGameManager.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class DelegateToGameManager : MonoBehaviour
 {
+    [SerializeField] private bool isGargoyle = false;
+
+    private void Start()
+    {
+        if (isGargoyle)
+        {
+            #if UNITY_WEBGL
+            Destroy(gameObject);
+            #endif
+        }
+    }
+
     public void DelegateQuitGame()
     {
         GameManager.instance.QuitGame();
